c5966bca2fd2bd88039e82b74f91a9e34a4d8be9
Dirk Dobson <dirkvancouver@live.com>
2018-10-23 11:18:50 -0600

    delete main.js styles.css

--- src/components/Ledger.js
+++ src/components/Ledger.js
@@ -1,6 +1,10 @@
 import React from 'react'
-
+import styled from 'styled-components'
 import Transactions from './Transactions'
+
+const Container = styled.div`
+  flex: 2
+`
 
 const Ledger = () => (
   <div>
