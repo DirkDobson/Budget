2a6b3932c8de557b0db5dfe9431603926a15276b
Dirk Dobson <dirkvancouver@live.com>
2018-10-23 10:00:21 -0600

    styled components

--- src/components/LedgerForm.js
+++ src/components/LedgerForm.js
@@ -1,3 +1,11 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addEntry } from '../reducers/ledger'
+import {
+  Input,
+  Button,
+  Flex,
+  FormBox,
+} from './Sharded'
 
 export default () => <div></div>
