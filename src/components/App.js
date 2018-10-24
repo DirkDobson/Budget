96ca9db69cfff55dc7822aafea0bf73b4e818669
Dirk Dobson <dirkvancouver@live.com>
2018-10-24 11:19:14 -0600

    name in body contact form

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 import { ProtectedRoute } from '@devpoint/dps-react-kit'
 import Home from './Home'
 import Auth from './Auth'
